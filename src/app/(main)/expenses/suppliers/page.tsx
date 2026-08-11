293668e486caa7c02d0b5fb72520dbdeba5ce64e
petrosphere-ayban <jmm@petrosphere.com.ph>
2026-08-11 16:42:44 +0800

    aw

--- src/app/(main)/expenses/suppliers/page.tsx
+++ src/app/(main)/expenses/suppliers/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { Suspense } from "react";
 import { Suppliers } from "@/views/suppliers";
 
 export default function Page() {
-  return <Suppliers />;
+  return (
+    <Suspense fallback={null}>
+      <Suppliers />
+    </Suspense>
+  );
 }
